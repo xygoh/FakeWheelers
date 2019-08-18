f51902c2cde5a2eb4356694c3063944332ce6f35
Alexander Ling <alexander@canva.com>
2019-08-18 23:38:28 +1000

    Init spring boot dir

---
+++ backend/src/main/java/com/fakewheelers/FakeWheelersApplication.java
@@ -0,0 +1,13 @@
+package com.fakewheelers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FakeWheelersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FakeWheelersApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/fakewheelers/FakeWheelersApplicationTests.java
@@ -0,0 +1,16 @@
+package com.fakewheelers;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FakeWheelersApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
